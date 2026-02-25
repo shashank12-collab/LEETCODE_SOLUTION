c7614d7c337f4c0a5900c6bbcbc4664ab75d271c
Shashank <shashank120956@gmail.com>
2026-02-25 19:10:08 +0530

    Time: 4 ms (56.89%) | Memory: 10.8 MB (92.49%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int l = 0;
+        int r = 0;
+        int maxlen = 0 ;
+        int maxf = 0;
+        int hash[26] = {0};
+        while(r < n)
+        {
+            hash[s[r] - 'A']++;
+            maxf = max(maxf , hash[s[r] - 'A']);
+
+            if((r-l+1)-maxf > k)
+            {
+                hash[s[l]-'A']--;
+                l = l + 1;
+            }
+
+            if((r-l+1)-maxf <= k)
+            {
+                maxlen = max(maxlen , r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
