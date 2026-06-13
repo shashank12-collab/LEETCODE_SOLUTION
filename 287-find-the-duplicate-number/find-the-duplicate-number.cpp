8d2a3a19414f393279e5db6cbe6abbca6e992e67
Shashank <shashank120956@gmail.com>
2026-06-13 15:49:09 +0530

    Time: 74 ms (11.08%) | Memory: 87.8 MB (15.96%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // unordered_set<int> st;
-        // int n = nums.size();
-        // for (int i = 0 ; i < n ; i++) {
-        //     if (st.find(nums[i]) != st.end()) {
-        //         return nums[i]; 
-        //     }
-        //     else
-        //     {
-        //     st.insert(nums[i]);
-        // }
-        // }
-        // return -1; 
+        unordered_set<int> st;
+        int n = nums.size();
+        for (int i = 0 ; i < n ; i++) {
+            if (st.find(nums[i]) != st.end()) {
+                return nums[i]; 
+            }
+            else
+            {
+            st.insert(nums[i]);
+        }
+        }
+        return -1; 
 
         //using slow and fast pointer 
-        int slow  = 0;
-        int fast = 0;
-        while(true)
-        {
-            slow = nums[slow];
-            fast = nums[fast];
-            fast = nums[fast];
-            if(slow == fast)
-            {
-                slow = 0;
-                while(slow != fast)
-                {
-                    slow = nums[slow];
-                    fast = nums[fast];
-                }
-                return slow;
-            }
-        }
-        return -1;
+    //     int slow  = 0;
+    //     int fast = 0;
+    //     while(true)
+    //     {
+    //         slow = nums[slow];
+    //         fast = nums[fast];
+    //         fast = nums[fast];
+    //         if(slow == fast)
+    //         {
+    //             slow = 0;
+    //             while(slow != fast)
+    //             {
+    //                 slow = nums[slow];
+    //                 fast = nums[fast];
+    //             }
+    //             return slow;
+    //         }
+    //     }
+    //     return -1;
+    // }
     }
 };
