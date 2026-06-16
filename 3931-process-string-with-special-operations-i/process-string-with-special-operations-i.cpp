451fac07a9b984fb2fe331ee71170a80ed022319
Shashank <shashank120956@gmail.com>
2026-06-16 11:37:22 +0530

    Time: 5 ms (45.83%) | Memory: 39.8 MB (69.70%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(s[i] >= 'a' && s[i] <= 'z')
+            {
+                ans.push_back(s[i]);
+            }
+            else if(s[i] == '#')
+            {
+                ans += ans;
+            }
+            else if(s[i] == '*')
+            {
+                if(!ans.empty())
+                {
+                    ans.pop_back();
+                }
+            }
+            else 
+            {
+                reverse(ans.begin() , ans.end());
+            }
+        }
+        return ans;
+    }
+};
