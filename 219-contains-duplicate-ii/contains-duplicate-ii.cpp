f7ef8d214028176579731161133978b565d3ebc9
Shashank <shashank120956@gmail.com>
2026-06-27 21:57:11 +0530

    Time: 1602 ms (5.10%) | Memory: 73.9 MB (99.14%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = i + 1 ; j < n && j <= i + k; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    if(abs(i - j) <= k)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
