5688bf23d1d6bb68a848dd35af89a9fa10108843
Shashank <shashank120956@gmail.com>
2026-07-04 23:23:42 +0530

    Time: 23 ms (3.50%) | Memory: 14.4 MB (6.24%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> NumToArr(int n)
+    {
+        vector<int> arr;
+        while(n >= 1)
+        {
+            int last_digit = n % 10;
+            arr.push_back(last_digit);
+            n = n / 10;
+        }
+        reverse(arr.begin() , arr.end());
+        return arr;
+    }
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> num;
+        for(int i = left ; i <= right ; i++)
+        {
+            vector<int> ans = NumToArr(i);
+            int j;
+            for(j = 0 ; j < ans.size() ; j++)
+            {
+               if(ans[j] == 0 || i % ans[j] != 0)
+                {
+                   break;
+                }
+            }
+             if(j == ans.size())
+             {
+                 num.push_back(i);
+             }    
+        }
+        return num;
+    }
+};
