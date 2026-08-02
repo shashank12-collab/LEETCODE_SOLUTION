256514d69ea52201aed38912bc59c9dbecc3a5ea
Shashank <shashank120956@gmail.com>
2026-08-02 17:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (34.92%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,8 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #BRUTE FORCE 
+        # n = len(nums)
+        # sorted(nums)
+        # mini = min(nums)
+        # return mini
+
+        #OPTIMAL CODE 
         n = len(nums)
-        sorted(nums)
-        mini = min(nums)
-        return mini
+        low = 0
+        high = n - 1 
+        ans = 10 ** 10
+        while(low <= high):
+            mid = int((low + high) / 2)
+            if(nums[low] <= nums[mid]):
+                ans = min(ans, nums[low])
+                low = mid + 1
+
+            else:
+                ans = min(ans, nums[mid])
+                high = mid - 1
+        return ans
+
         
